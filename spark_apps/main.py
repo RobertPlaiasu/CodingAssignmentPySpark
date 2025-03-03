c18c5315c020eeb53ea781ad831c64e8caf73a46
RobertPlaiasu <robertplaiasu03@gmail.com>
2025-03-04 00:16:55 +0200

    expand join

--- spark_apps/main.py
+++ spark_apps/main.py
@@ -16,7 +16,7 @@ def main():
                         sep=',',
                         quote='"',
                         escape='"'
-                   ))
+                        ))
 
     # facebook_df.show(5)
 
@@ -32,12 +32,15 @@ def main():
     # google_df.show(5)
 
     website_df = (spark.read
-                  .csv(data_folder + '/website_dataset.csv', header=True, inferSchema=True, sep=';'))
+                  .csv(data_folder + '/website_dataset.csv', header=True, inferSchema=True,
+                       sep=';',
+                       quote='"',
+                       escape='"'))
     # website_df.printSchema()
     # website_df.show(5)
 
-    # trim data and make columns with lower characters
-    google_df_trimmed = google_df.select(trim(google_df['address']).alias('address'),
+    # trim data and transform some columns to have lower characters
+    google_df_trimmed = google_df.select(lower(trim(google_df['address'])).alias('address'),
                                          lower(trim(google_df['category'])).alias('category'),
                                          lower(trim(google_df['city'])).alias('city'),
                                          lower(trim(google_df['country_code'])).alias('country_code'),
@@ -45,16 +48,16 @@ def main():
                                          trim(google_df['name']).alias('name'),
                                          trim(google_df['phone']).alias('phone'),
                                          trim(google_df['phone_country_code']).alias('phone_country_code'),
-                                         trim(google_df['raw_address']).alias('raw_address'),
+                                         lower(trim(google_df['raw_address'])).alias('raw_address'),
                                          trim(google_df['raw_phone']).alias('raw_phone'),
                                          lower(trim(google_df['region_code'])).alias('region_code'),
                                          lower(trim(google_df['region_name'])).alias('region_name'),
                                          trim(google_df['text']).alias('text'),
-                                         trim(google_df['zip_code']).alias('zip_code'),
+                                         lower(trim(google_df['zip_code'])).alias('zip_code'),
                                          lower(trim(google_df['domain'])).alias('domain'))
 
     facebook_df_trimmed = facebook_df.select(lower(trim(facebook_df['domain'])).alias('domain'),
-                                             trim(facebook_df['address']).alias('address'),
+                                             lower(trim(facebook_df['address'])).alias('address'),
                                              lower(trim(facebook_df['categories'])).alias('categories'),
                                              lower(trim(facebook_df['city'])).alias('city'),
                                              lower(trim(facebook_df['country_code'])).alias('country_code'),
@@ -68,7 +71,7 @@ def main():
                                              trim(facebook_df['phone_country_code']).alias('phone_country_code'),
                                              lower(trim(facebook_df['region_code'])).alias('region_code'),
                                              lower(trim(facebook_df['region_name'])).alias('region_name'),
-                                             trim(facebook_df['zip_code']).alias('zip_code'))
+                                             lower(trim(facebook_df['zip_code'])).alias('zip_code'))
 
     website_df_trimmed = website_df.select(lower(trim(website_df['root_domain'])).alias('root_domain'),
                                            lower(trim(website_df['domain_suffix'])).alias('domain_suffix'),
@@ -80,7 +83,7 @@ def main():
                                            trim(website_df['phone']).alias('phone'),
                                            trim(website_df['site_name']).alias('site_name'),
                                            lower(trim(website_df['tld'])).alias('tld'),
-                                           trim(website_df['s_category']).alias('s_category'))
+                                           lower(trim(website_df['s_category'])).alias('s_category'))
 
     # verify if exist rows with name null
     # print(google_df_trimmed.filter(col('name').isNull()).count())
@@ -129,49 +132,76 @@ def main():
     facebook_df_trimmed = facebook_df_trimmed.filter(col('domain').contains('.'))
     google_df_trimmed_cleared = google_df_trimmed_cleared.filter(col('domain').contains('.'))
 
+    # search for connection between facebook_df and google_df
+    print(facebook_df_trimmed.alias('a').join(google_df_trimmed_cleared.alias('b'),
+                                              col('a.domain') == col('b.domain'), 'full')
+          .filter(col('a.address').isNotNull() &
+                  ((col('a.address') == col('b.address')) | (col('a.address') == col('b.raw_address')))).count())
+    print(facebook_df_trimmed.alias('a').join(google_df_trimmed_cleared.alias('b'),
+                                              col('a.domain') == col('b.domain'), 'full')
+          .filter(col('a.address').isNotNull()
+                  & (col('a.address') == col('b.raw_address'))).count())
+
     (google_df_trimmed_cleared.alias('a').join(facebook_df_trimmed.alias('b'),
-                                               col('a.domain') == col('b.domain'), "full")
+                                               (col('a.domain') == col('b.domain'))
+                                               & ((col('a.name') == col('b.name')) | col('a.name').isNull()
+                                                  | col('b.name').isNull())
+                                               & ((col('a.zip_code') == col('b.zip_code')) | col('a.zip_code').isNull() |
+                                                  col('b.zip_code').isNull())
+                                               & ((col('a.address') == col('b.address')) | col('a.address').isNull() |
+                                                  col('b.address').isNull())
+                                               & ((col('a.phone') == col('b.phone')) | col('a.phone').isNull()
+                                                  | col('b.phone').isNull())
+                                               & ((col('a.city') == col('b.city')) | col('a.city').isNull() |
+                                                  col('b.city').isNull())
+                                               & ((col('a.country_name') == col('b.country_name')) |
+                                                  col('a.country_name').isNull() | col('b.country_name').isNull()
+                                                  | (col('a.country_code') == col('b.country_code')) |
+                                                  col('a.country_code').isNull() | col('b.country_code').isNull())
+                                               & ((col('a.region_name') == col('b.region_name'))
+                                                  | col('a.region_name').isNull() | col('b.region_name').isNull()
+                                                  | (col('a.region_code') == col('b.region_code'))
+                                                  | col('a.region_code').isNull() | col('b.region_code').isNull())
+                                               , "full")
      .join(website_df_trimmed_clear.alias('c'),
            col('c.root_domain') == col('a.domain'), 'full')
-     .select(col('b.link').alias('link'),
-             col('b.page_type').alias('page_type'),
-             col('b.email').alias('page_type'),
-             col('a.raw_phone').alias('raw_phone'),
-             col('c.language').alias('language'),
-             col('c.legal_name').alias('legal_name'),
-             col('b.phone_country_code').alias('phone_country_code'),
-             col('c.domain_suffix').alias('old_domain_suffix'),
-             col('c.tld').alias('old_tld'),
-             when(col('a.text').isNotNull(), col('a.text'))
-             .otherwise(col('b.description')).alias('description'),
-             when(col('a.zip_code').isNotNull(), col('a.zip_code'))
-             .otherwise(col('b.zip_code')).alias('zip_code'),
-             when(col('a.region_code').isNotNull(), col('a.region_code'))
-             .otherwise(col('b.region_code')).alias('region_code'),
-             when(col('a.region_name').isNotNull(), col('a.region_name'))
-             .when(col('c.main_region').isNotNull(), col('c.main_region'))
-             .otherwise(col('b.region_name')).alias('region_name'),
-             when(col('a.city').isNotNull(), col('a.city'))
-             .when(col('c.main_city').isNotNull(), col('c.main_city'))
-             .otherwise(col('b.city')).alias('city'),
-             when(col('a.country_code').isNotNull(), col('a.country_code'))
-             .otherwise(col('b.country_code')).alias('country_code'),
-             when(col('a.country_name').isNotNull(), col('a.country_name'))
-             .when(col('c.main_country').isNotNull(), col('c.main_country'))
-             .otherwise(col('b.country_name')).alias('country_name'),
-             when(col('a.raw_address').isNotNull(), col('a.raw_address'))
-             .otherwise(col('b.address')).alias('address'),
-             when(col('a.domain').isNotNull(), col('a.domain'))
-             .when(col('c.root_domain').isNotNull(), col('c.root_domain'))
-             .otherwise(col('b.domain')).alias('domain'),
-             when(col('a.phone').isNotNull(), col('a.phone'))
-             .when(col('c.phone').isNotNull(), col('c.phone'))
-             .otherwise(col('b.phone')).alias('phone'),
-             when(col('a.category').isNotNull(), col('a.category'))
-             .when(col('c.s_category').isNotNull(), col('c.s_category'))
-             .otherwise(col('b.categories')).alias('category'),
-             when(col('a.name').isNotNull(), col('a.name'))
-             .otherwise(col('b.name')).alias('name'))
+     .select(
+        col('a.raw_phone').alias('raw_phone'),
+        col('b.phone_country_code').alias('phone_country_code'),
+        col('c.domain_suffix').alias('old_domain_suffix'),
+        col('c.tld').alias('old_tld'),
+        when(col('a.text').isNotNull(), col('a.text'))
+        .otherwise(col('b.description')).alias('description'),
+        when(col('a.zip_code').isNotNull(), col('a.zip_code'))
+        .otherwise(col('b.zip_code')).alias('zip_code'),
+        when(col('a.region_code').isNotNull(), col('a.region_code'))
+        .otherwise(col('b.region_code')).alias('region_code'),
+        when(col('a.region_name').isNotNull(), col('a.region_name'))
+        .when(col('c.main_region').isNotNull(), col('c.main_region'))
+        .otherwise(col('b.region_name')).alias('region_name'),
+        when(col('a.city').isNotNull(), col('a.city'))
+        .when(col('c.main_city').isNotNull(), col('c.main_city'))
+        .otherwise(col('b.city')).alias('city'),
+        when(col('a.country_code').isNotNull(), col('a.country_code'))
+        .otherwise(col('b.country_code')).alias('country_code'),
+        when(col('a.country_name').isNotNull(), col('a.country_name'))
+        .when(col('c.main_country').isNotNull(), col('c.main_country'))
+        .otherwise(col('b.country_name')).alias('country_name'),
+        when(col('a.raw_address').isNotNull(), col('a.raw_address'))
+        .otherwise(col('b.address')).alias('address'),
+        when(col('a.domain').isNotNull(), col('a.domain'))
+        .when(col('c.root_domain').isNotNull(), col('c.root_domain'))
+        .otherwise(col('b.domain')).alias('domain'),
+        when(col('a.phone').isNotNull(), col('a.phone'))
+        .when(col('c.phone').isNotNull(), col('c.phone'))
+        .otherwise(col('b.phone')).alias('phone'),
+        when(col('a.category').isNotNull(), col('a.category'))
+        .when(col('c.s_category').isNotNull(), col('c.s_category'))
+        .otherwise(col('b.categories')).alias('category'),
+        when(col('a.name').isNotNull(), col('a.name'))
+        .when(col('c.site_name').isNotNull(), col('c.site_name'))
+        .when(col('b.name').isNotNull(), col('b.name'))
+        .otherwise(col('c.legal_name')).alias('name'))
      .withColumn('domain_suffix', when(col('old_domain_suffix').isNotNull(), col('old_domain_suffix'))
                  .otherwise(split('domain', '.', 1)[1]))
      .drop(col('old_domain_suffix'))
